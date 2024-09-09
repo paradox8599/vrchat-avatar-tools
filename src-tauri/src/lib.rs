2aaf11f6dad8d49b71622c4f43ed06e9d0c4fef6
paradox8599 <paradox8599@outlook.com>
2024-09-09 20:33:17 +1000

    added tauri plugins

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,9 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .plugin(tauri_plugin_store::Builder::new().build())
+        .plugin(tauri_plugin_shell::init())
+        .plugin(tauri_plugin_clipboard_manager::init())
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
