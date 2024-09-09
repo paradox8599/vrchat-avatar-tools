3470fea10af0e16218b29508f1ca8411e2ae9463
paradox8599 <paradox8599@outlook.com>
2024-09-10 00:41:15 +1000

    fix: avatar expire time

--- src/hooks/useAvatarFetcher.ts
+++ src/hooks/useAvatarFetcher.ts
@@ -5,12 +5,14 @@ import { vrchatGetAvatarInfo } from "@/lib/api";
 function hasOutdated(date?: Date | string) {
   return (
     Date.now() - new Date(date ?? 0)?.getTime() >
-    appState.settings.avatarStatusExpiresHr
+    appState.settings.avatarStatusExpiresHr * 60 * 60 * 1000
   );
 }
 
 function getOutdatedAvatar() {
-  return appState.avatars.find((a) => !a.lastFetch || hasOutdated(a.lastFetch));
+  return appState.avatars.filter(
+    (a) => !a.lastFetch || hasOutdated(a.lastFetch),
+  )[0];
 }
 
 export function useAvatarFetcher() {
