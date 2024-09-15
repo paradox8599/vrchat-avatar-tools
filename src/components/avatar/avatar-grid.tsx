d34ea6e9ff28f7fa50b57a79fbfa6da82fab16f2
paradox8599 <paradox8599@outlook.com>
2024-09-15 18:50:28 +1000

    feat(avatar): delete button in grid card

--- src/components/avatar/avatar-grid.tsx
+++ src/components/avatar/avatar-grid.tsx
@@ -10,10 +10,17 @@ import {
 import useAvatars from "@/hooks/useAvatars";
 import { useAvatarFetcher } from "@/hooks/useAvatarFetcher";
 import { Button } from "../ui/button";
-import { Box, CloudUpload, Copy, SquareArrowOutUpRight } from "lucide-react";
+import {
+  Box,
+  CloudUpload,
+  Copy,
+  SquareArrowOutUpRight,
+  TrashIcon,
+} from "lucide-react";
 import { writeText } from "@tauri-apps/plugin-clipboard-manager";
 import { toast } from "@/hooks/use-toast";
 import { AvatarTagSelector } from "./tag-selector";
+import { avatarMapState } from "@/state/avatars";
 
 export default function AvatarGrid() {
   const { sortedAvatars } = useAvatars();
@@ -37,99 +44,101 @@ export default function AvatarGrid() {
             <Card
               key={avatar.id}
               className={cn(
-                "max-w-fit min-w-fit min-h-[8rem] py-2 px-2",
+                "max-w-fit min-w-fit min-h-[8rem]",
+                "px-2 flex flex-col justify-center gap-2",
                 avatar.info ? "bg-cardhl" : "bg-card",
               )}
             >
-              <div className="flex flex-col gap-2">
-                <div className="flex items-start justify-start gap-2">
-                  {/* thumbnail image */}
+              {/* thumbnail & id row */}
 
-                  <AvatarIcon>
-                    <AvatarImage
-                      src={avatar.info?.thumbnailImageUrl}
-                      className="object-cover"
-                    />
-                    <AvatarFallback />
-                  </AvatarIcon>
+              <div className="flex items-start justify-start gap-2">
+                {/* thumbnail image */}
 
-                  {/* avatar id */}
+                <AvatarIcon>
+                  <AvatarImage
+                    src={avatar.info?.thumbnailImageUrl}
+                    className="object-cover"
+                  />
+                  <AvatarFallback />
+                </AvatarIcon>
 
-                  <div className="w-full flex items-center justify-center">
+                {/* avatar id */}
+
+                <div className="w-full flex items-center justify-center">
+                  <Button
+                    className={cn(
+                      "rounded-full bg-background",
+                      "font-mono font-bold text-xs",
+                      "flex gap-2",
+                    )}
+                    variant="secondary"
+                    onClick={() =>
+                      writeText(avatar.id).then(() =>
+                        toast({
+                          title: "已复制模型ID",
+                          description: avatar.id,
+                        }),
+                      )
+                    }
+                  >
+                    {avatar.id}
+                    <Copy size={16} />
+                  </Button>
+                </div>
+              </div>
+
+              {/* info row */}
+
+              <div className="w-full text-sm flex flex-col gap-1">
+                <div className="flex items-center justify-between">
+                  {/* vrchat urls */}
+                  <div className="flex items-center justify-start gap-4">
+                    {/* avatar status */}
+
                     <Button
                       className={cn(
-                        "rounded-full bg-background",
-                        "font-mono font-bold text-xs",
-                        "flex gap-2",
+                        "uppercase w-24 rounded-full font-bold flex gap-2",
+                        avatar.info ? "" : "bg-muted text-foreground",
                       )}
-                      variant="secondary"
                       size="sm"
                       onClick={() =>
-                        writeText(avatar.id).then(() =>
-                          toast({
-                            title: "已复制模型ID",
-                            description: avatar.id,
-                          }),
-                        )
+                        open(`https://vrchat.com/home/avatar/${avatar.id}`)
                       }
                     >
-                      {avatar.id}
-                      <Copy size={16} />
+                      {avatar.info?.releaseStatus ? "公开" : "未知"}
+                      <SquareArrowOutUpRight size={12} />
                     </Button>
-                  </div>
-                </div>
 
-                {/* info */}
+                    {/* avatar author name  */}
 
-                <div className="w-full text-sm flex flex-col gap-1">
-                  <div className="flex items-center justify-between">
-                    {/* vrchat urls */}
-                    <div className="flex items-center justify-start gap-4">
-                      {/* avatar status */}
-
+                    {avatar.info && (
                       <Button
-                        className={cn(
-                          "uppercase w-24 rounded-full font-bold flex gap-2",
-                          avatar.info ? "" : "bg-muted text-foreground",
-                        )}
+                        className="rounded-full text-md flex gap-1 font-bold"
                         size="sm"
                         onClick={() =>
-                          open(`https://vrchat.com/home/avatar/${avatar.id}`)
+                          open(
+                            `https://vrchat.com/home/user/${avatar.info?.authorId}`,
+                          )
                         }
                       >
-                        {avatar.info?.releaseStatus ? "公开" : "未知"}
+                        {avatar.info?.authorName}
                         <SquareArrowOutUpRight size={12} />
                       </Button>
+                    )}
+                  </div>
 
-                      {/* avatar author name  */}
+                  {/* tag selector */}
 
-                      {avatar.info && (
-                        <Button
-                          className="rounded-full text-md flex gap-1 font-bold"
-                          size="sm"
-                          onClick={() =>
-                            open(
-                              `https://vrchat.com/home/user/${avatar.info?.authorId}`,
-                            )
-                          }
-                        >
-                          {avatar.info?.authorName}
-                          <SquareArrowOutUpRight size={12} />
-                        </Button>
-                      )}
-                    </div>
-
-                    {/* tag selector */}
-
-                    <div className="flex-center">
-                      <AvatarTagSelector avatar={avatar} />
-                    </div>
+                  <div className="flex-center">
+                    <AvatarTagSelector avatar={avatar} />
                   </div>
                 </div>
+              </div>
 
-                {/* dates */}
+              <div className="flex justify-between items-center gap-1">
+                {/* dates & delete row */}
 
-                <div className="font-mono flex items-center justify-between text-sm">
+                <div className="font-mono flex items-center justify-between text-sm gap-2">
                   <p className="flex-center gap-2 bg-accent text-accent-foreground px-2 rounded-full">
                     <Box size={13} />
                     {avatar.info
@@ -143,6 +152,16 @@ export default function AvatarGrid() {
                       : "----/--/-- --:--"}
                   </p>
                 </div>
+
+                {/* delete button */}
+                <Button
+                  variant="ghost"
+                  size="icon"
+                  className="flex-1 h-5"
+                  onClick={() => avatarMapState.delete(avatar.id)}
+                >
+                  <TrashIcon size={16} />
+                </Button>
               </div>
             </Card>
           );
