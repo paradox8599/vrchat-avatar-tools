f01d6d1bb147249dbcb2d7dea8b94ddb8b240428
paradox8599 <paradox8599@outlook.com>
2024-09-19 01:06:59 +1000

    fix: switch initial page back to home

--- src/providers/init-provider.tsx
+++ src/providers/init-provider.tsx
@@ -38,7 +38,7 @@ export default function InitProvider({
     if (!appState.init) {
       router.replace("/splash");
     } else if (path === "/splash") {
-      router.push("/settings");
+      router.push("/");
     }
   }, [init, path, router]);
 
