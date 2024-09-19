6ce44de7c6cb76e33c758d32eb59a91465fe58fe
paradox8599 <paradox8599@outlook.com>
2024-09-19 23:51:06 +1000

    feat: prevent context menu on release

--- src/hooks/useAppInit.ts
+++ src/hooks/useAppInit.ts
@@ -37,4 +37,20 @@ export default function useAppInit() {
       router.push("/");
     }
   }, [init, path, router]);
+
+  React.useEffect(() => {
+    if (window.location.hostname !== 'tauri.localhost') {
+      return
+    }
+
+    document.addEventListener('contextmenu', e => {
+      e.preventDefault();
+      return false;
+    }, { capture: true })
+
+    document.addEventListener('selectstart', e => {
+      e.preventDefault();
+      return false;
+    }, { capture: true })
+  }, [])
 }
