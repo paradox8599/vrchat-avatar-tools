d427e7cc00fd464ffdfc8f1e05b6c582bc0b5a5d
paradox8599 <paradox8599@outlook.com>
2024-09-23 01:19:40 +1000

    ci(build): load key & password from env

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,3 +1,4 @@
-$env:TAURI_SIGNING_PRIVATE_KEY=$(cat $HOME\.tauri\app.key)
+$env:TAURI_SIGNING_PRIVATE_KEY=$(bun -e 'console.log(process.env.TAURI_SIGNING_PRIVATE_KEY)')
+$env:TAURI_SIGNING_PRIVATE_KEY_PASSWORD=$(bun -e 'console.log(process.env.TAURI_SIGNING_PRIVATE_KEY_PASSWORD)')
 cargo tauri build
 
