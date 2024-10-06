21d0ab0db6102d6f0986798840d34d9c79d10b09
paradox8599 <paradox8599@outlook.com>
2024-10-06 16:25:34 +1100

    refactor: renamed app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,8 @@ import AppProvider from "@/app/providers";
 import NavBar from "@/components/navigation/nav-bar";
 
 export const metadata: Metadata = {
-  title: "VRChat 模型公开检测",
-  description: "自动定期批量检查 VRChat 模型公开状态",
+  title: "VRChat 模型工具",
+  description: "VRChat 模型相关工具",
 };
 
 export default function RootLayout({
