1ea573943bbf36da1f3caa8dbafad6e9a0324f61
paradox8599 <paradox8599@outlook.com>
2024-10-06 15:54:04 +1100

    feat: rm default fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,6 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 import AppProvider from "@/app/providers";
-import { cn } from "@/lib/utils";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata: Metadata = {
   title: "VRChat 模型公开检测",
@@ -22,12 +9,10 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={cn(geistSans.variable, geistMono.variable)}>
+      <body>
         <AppProvider>{children}</AppProvider>
       </body>
     </html>
